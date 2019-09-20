c89018f80ebf4539e9e696ae441819bea31214ef
arfaniswan <arfaniswan@gmail.com>
2019-09-20 19:48:44 +0100

    local notifications

--- confusion/components/ReservationComponent.js
+++ confusion/components/ReservationComponent.js
@@ -2,7 +2,8 @@
 import React, { Component } from 'react';
 import { Text, View, ScrollView, StyleSheet, Picker, Switch, Button, Alert } from 'react-native';
 import * as Animatable from 'react-native-animatable';
-
+import {  Notifications } from 'expo';
+import * as Permissions from 'expo-permissions';
 import DatePicker from 'react-native-datepicker';
 
 
@@ -41,7 +42,9 @@ class Reservation extends Component {
                
               },
               { text: "OK", 
-              onPress: () => this.resetForm() }
+              onPress: () => {
+                this.presentLocalNotification(this.state.date);
+                this.resetForm();} }
             ],
             { cancelable: false }
           );
@@ -54,6 +57,34 @@ class Reservation extends Component {
             smoking: false,
             date: '',
             
+        });
+    }
+
+
+    async obtainNotificationPermission() {
+        let permission = await Permissions.getAsync(Permissions.USER_FACING_NOTIFICATIONS);
+        if (permission.status !== 'granted') {
+            permission = await Permissions.askAsync(Permissions.USER_FACING_NOTIFICATIONS);
+            if (permission.status !== 'granted') {
+                Alert.alert('Permission not granted to show notifications');
+            }
+        }
+        return permission;
+    }
+
+    async presentLocalNotification(date) {
+        await this.obtainNotificationPermission();
+        Notifications.presentLocalNotificationAsync({
+            title: 'Your Reservation',
+            body: 'Reservation for '+ date + ' requested',
+            ios: {
+                sound: true
+            },
+            android: {
+                sound: true,
+                vibrate: true,
+                color: '#512DA8'
+            }
         });
     }
     
