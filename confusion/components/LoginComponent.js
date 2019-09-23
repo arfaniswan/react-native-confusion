95b556a193f88b9f5e9af9a1149c8b2ca69a2e63
arfaniswan <arfaniswan@gmail.com>
2019-09-23 11:55:55 +0100

    image manipulator added

--- confusion/components/LoginComponent.js
+++ confusion/components/LoginComponent.js
@@ -5,7 +5,7 @@ import { createBottomTabNavigator } from 'react-navigation';
 import { baseUrl } from '../shared/baseUrl';
 import * as SecureStore from 'expo-secure-store';
 import * as ImagePicker from 'expo-image-picker';
-
+import * as ImageManipulator from 'expo-image-manipulator';
 import * as Permissions from 'expo-permissions';
 class LoginTab extends Component {
 
@@ -145,9 +145,21 @@ class RegisterTab extends Component {
             });
             if (!capturedImage.cancelled) {
                 console.log(capturedImage);
-                this.setState({imageUrl: capturedImage.uri });
+                this.processImage(capturedImage.uri);
             }
         }
+
+    }
+    processImage = async (imageUri) => {
+        let  processedImage = await ImageManipulator.manipulateAsync(
+            imageUri, 
+            [
+                {resize: {width: 400}}
+            ],
+            {format: 'png'}
+        );
+        console.log(processedImage);
+        this.setState({imageUrl: processedImage.uri });
 
     }
     
